ecd432769dba18a05a3d491e5e77bc965de2a62f
Muhammad Rafi <iamyawpie@protonmail.com>
2024-01-10 21:02:11 +0800

    3

--- myApp.js
+++ myApp.js
@@ -4,7 +4,8 @@ let app = express();
 console.log("Hello World");
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  let path = __dirname + "/views/index.html";
+  res.sendFile(path);
 });
 
 module.exports = app;
