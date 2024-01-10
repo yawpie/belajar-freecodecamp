775af1a16d4b5c57c132929fb425c7b6cfe6d2c0
Muhammad Rafi <iamyawpie@protonmail.com>
2024-01-10 21:04:15 +0800

    4

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@ let express = require("express");
 let app = express();
 
 console.log("Hello World");
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   let path = __dirname + "/views/index.html";
